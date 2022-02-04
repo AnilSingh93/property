288b30ff56e3cca7cc2df3c216e2a2dea4d0db8a
AnilSingh93 <80147252+AnilSingh93@users.noreply.github.com>
2022-02-04 20:05:25 +0530

    added

--- js/custom.js
+++ js/custom.js
@@ -186,6 +186,41 @@ jQuery(document).ready(function($) {
 	]
 	
 });
+$(".homepage-property-slider").slick({
+  infinite: true,
+slidesToShow: 4,
+slidesToScroll: 1,
+arrows: false,
+autoplay: true,
+autoplaySpeed: 2000,
+dots:false,
+responsive: [
+{
+breakpoint: 1024,
+settings: {
+slidesToShow: 3,
+slidesToScroll: 3,
+}
+},
+{
+breakpoint: 768,
+settings: {
+slidesToShow: 2,
+slidesToScroll: 1
+}
+},
+{
+breakpoint: 601,
+settings: {
+slidesToShow: 1,
+slidesToScroll: 1
+}
+}
+
+]
+
+});
+
 });
 
 
